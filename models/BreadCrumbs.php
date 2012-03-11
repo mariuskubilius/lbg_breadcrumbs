38e6cebe569bdf1eb9bae82aa1db1c728dbf1422
Marius Kubilius <marius.kubilius@gmail.com>
2012-03-11 20:31:46 +0200

    schema refactoring

--- models/BreadCrumbs.php
+++ models/BreadCrumbs.php
@@ -5,6 +5,7 @@ class BreadCrumbs extends \lithium\data\model {
 	
 	protected $_shchema = array(
 		'_id' => array('type' => 'id'),
+		'adapter' => array('type' => 'string'),
 		'url' => array('type' => 'string'),
 		'params' => array('type' => 'string', 'array' => true, 'default' => array()),
 		'trail' => array('type' => 'string', 'array' => true, 'default' => array()),
