406674050bac7879fc2fa04a0f8e19f6fcf5fc61
Marius Kubilius <marius.kubilius@gmail.com>
2012-03-11 19:22:19 +0200

    initial schema definition

--- models/BreadCrumbs.php
+++ models/BreadCrumbs.php
@@ -4,7 +4,10 @@ namespace mg_breadcrumbs\models;
 class BreadCrumbs extends \lithium\data\model {
 	
 	protected $_shchema = array(
-		'_id' => array('type' => 'id');
+		'_id' => array('type' => 'id'),
+		'url' => array('type' => 'string'),
+		'params' => array('type' => 'string', 'array' => true, 'default' => array()),
+		'trail' => array('type' => 'string', 'array' => true, 'default' => array()),
 	);
 }
 ?>
