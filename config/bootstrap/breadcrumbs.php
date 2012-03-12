4d4ce38af323a507aeccf46994a0e20d8bfc82b5
Marius Kubilius <marius.kubilius@gmail.com>
2012-03-12 02:05:19 +0200

    final changes in config.

--- config/bootstrap/breadcrumbs.php
+++ config/bootstrap/breadcrumbs.php
@@ -8,12 +8,6 @@ use mg_breadcrumbs\navigation\BreadCrumbs;
 BreadCrumbs::config(array(
 	'frontend' => array(
 		'adapter' => 'RouteParams',
-		'separator' => '/',
-		'home' => array(
-			'enabled' => true,
-			'title' => 'Home',
-			'url' => 'resorts',
-		),
 		'trail' => array(
 			'location' => array(
 				'params' => array(
@@ -58,10 +52,4 @@ BreadCrumbs::config(array(
 			),
 		),
 )));
-/**
- * 'location' => string 'switzerland' (length=11)
- *	'resort' => string 'gstaad' (length=6)
- *	'category' => string 'tourist-information' (length=19)
- *	'entry' => string 'gstaad-info' (length=11)
- */
 
